83ffceba436159317ff25f36b92348c2b2ac627f
Mahbub Hassan <mahbub.hassan@nielsen.com>
2020-06-29 21:33:07 +0600

    support helm3

--- assets/common.sh
+++ assets/common.sh
@@ -17,24 +17,8 @@ setup_kubernetes() {
 }
 
 setup_helm() {
-    init_server=$(jq -r '.source.helm_init_server // "false"' < $1)
     namespace=$(jq -r '.source.namespace // "default"' < $1)
-    tiller_namespace=$(jq -r '.source.tiller_namespace // "kube-system"' < $1)
-    helm_request_base="helm --tiller-namespace $tiller_namespace"
-    
-    if [ "$init_server" = true ]; then
-        tiller_service_account=$(jq -r '.source.tiller_service_account // "default"' < $1)
-
-        sed -i -e 's/((tiller-namespace))/'$tiller_namespace'/g' /opt/resource/role-tiller.yml
-        sed -i -e 's/((tiller-namespace))/'$tiller_namespace'/g' /opt/resource/rolebinding-tiller.yml
-        kubectl apply -f /opt/resource/role-tiller.yml --namespace $namespace
-        kubectl apply -f /opt/resource/rolebinding-tiller.yml --namespace $namespace
-
-        helm init --tiller-namespace=$tiller_namespace --service-account=$tiller_service_account --upgrade
-        wait_for_service_up tiller-deploy 10
-    else
-        helm init -c --tiller-namespace $tiller_namespace > /dev/null
-    fi
+    helm_request_base="helm -n $namespace"
     
     ca_cert=$(jq -r '.source.ca_cert // ""' < $1)
     client_cert=$(jq -r '.source.client_cert // ""' < $1)
@@ -55,11 +39,10 @@ setup_helm() {
         echo "$client_cert" > $(helm home)/cert.pem
         echo "$client_key" > $(helm home)/key.pem
         
-        helm_request_base="helm --tiller-namespace $tiller_namespace \
-        --tls --tls-verify \
-        --tls-ca-cert $(helm home)/ca.pem \
-        --tls-cert $(helm home)/cert.pem \
-        --tls-key $(helm home)/key.pem"
+        helm_request_base="helm -n $namespace \
+        --ca-file $(helm home)/ca.pem \
+        --cert-file $(helm home)/cert.pem \
+        --key-file $(helm home)/key.pem"
     fi
 }
 
@@ -77,23 +60,9 @@ wait_for_service_up() {
     fi
 }
 
-setup_repos() {
-    repos=$(jq -r '(try .source.repos[] catch [][]) | (.name+" "+.url)' < $1)
-    tiller_namespace=$(jq -r '.source.tiller_namespace // "kube-system"' < $1)
-    
-    IFS=$'\n'
-    for r in $repos; do
-        name=$(echo $r | cut -f1 -d' ')
-        url=$(echo $r | cut -f2 -d' ')
-        echo Installing helm repository $name $url
-        helm repo add --tiller-namespace $tiller_namespace $name $url
-    done
-}
-
 setup_resource() {
     echo "Initializing kubectl..."
     setup_kubernetes $1
     echo "Initializing helm..."
     setup_helm $1
-    setup_repos $1
 }
